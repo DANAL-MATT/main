0d3daf33272e90149daa28bf01041e7602b61ea3
Cadenza <mdfordomain@gmail.com>
2022-03-31 13:07:45 +0800

    Commit for ftest and unit, for latest version

--- GotchaSys/tests.py
+++ GotchaSys/tests.py
@@ -1,8 +1,18 @@
 from django.urls import resolve
 from django.test import TestCase
 from GotchaSys.views import MainPage
+from django.http import HttpRequest
+
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_mainpage_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, MainPage)
+		
+	def test_mainpage_returns_correct_view(self):
+		request = HttpRequest()
+		response = MainPage(request)
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>Contact Tracing List</title>', html)
+		self.assertTrue(html.endswith('</html>'))
 # Create your tests here.
--- GotchaSys/views.py
+++ GotchaSys/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def MainPage():
-	pass
+def MainPage(request):
+	return HttpResponse('<html><title>Contact Tracing List</title></html>')
 # Create your views here.
