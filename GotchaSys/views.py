d6a4f935de8528d49b9535634ff05e60769b4f2b
Cadenza <mdfordomain@gmail.com>
2022-04-02 21:44:57 +0800

    For new saving, April 2

--- GotchaSys/views.py
+++ GotchaSys/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def MainPage(request):
-	return HttpResponse('<html><title>Contact Tracing List</title></html>')
+	return render(request, 'mainpage.html')
 # Create your views here.
